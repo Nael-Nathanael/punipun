4d339a349920c64533df4c9fd1b7abc249b48831
Nathanael <nael.nathanael71@gmail.com>
2021-09-27 14:51:44 +0800

    feat: setup general layout

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<header>
+
+</header>
+
+
+<style>
+    header {
+        height: 100vh;
+        background: linear-gradient(0deg, rgba(53, 19, 108, 1) 0%, rgba(53, 19, 108, .8) 100%);
+    }
+</style>
